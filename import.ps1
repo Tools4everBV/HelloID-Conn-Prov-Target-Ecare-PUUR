e96486ad8038c77de919db563571bfe22344714c
rscholtelubberink <80954753+rscholtelubberink@users.noreply.github.com>
2025-07-21 13:28:47 +0200

    Added Import.ps1
    Added Import script. Permissions will follow shortly

---
+++ import.ps1
@@ -0,0 +1,214 @@
+#################################################
+# HelloID-Conn-Prov-Target-Ecare-Import
+# PowerShell V2
+#################################################
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+#region functions
+function Get-GenericScimOAuthToken {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ClientID,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ClientSecret,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $TokenUrl
+    )
+    try {
+
+        $headers = @{
+            'content-type' = 'application/x-www-form-urlencoded'
+        }
+
+        $body = @{
+            client_id     = $ClientID
+            client_secret = $ClientSecret
+            grant_type    = 'client_credentials'
+            scope         = "Ecare.Service.SCIM"
+        }
+
+        $splatParams = @{
+            Uri     = "$($TokenUrl)/connect/token"
+            Method  = 'POST'
+            Headers = $headers
+            Body    = $body
+        }
+
+        $Response = Invoke-RestMethod @splatParams
+        Write-Output $Response.access_token
+
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($PSItem)
+    }
+}
+function Invoke-EcareRestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Method,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Uri,
+
+        [object]
+        $Body,
+
+        [string]
+        $ContentType = 'application/json',
+
+        [Parameter(Mandatory = $false)]
+        [System.Collections.IDictionary]
+        $Headers = @{}
+    )
+
+    process {
+        try {
+            $splatParams = @{
+                Uri         = $Uri
+                Headers     = $headers
+                Method      = $Method
+                ContentType = $ContentType
+            }
+
+            if ($Body) {
+                $splatParams['Body'] = $Body
+            }
+            Invoke-RestMethod @splatParams -Verbose:$false
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
+}
+function Resolve-EcareError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+        if (-not [string]::IsNullOrEmpty($ErrorObject.ErrorDetails.Message)) {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+        }
+        elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -ne $ErrorObject.Exception.Response) {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                if (-not [string]::IsNullOrEmpty($streamReaderResponse)) {
+                    $httpErrorObj.ErrorDetails = $streamReaderResponse
+                }
+            }
+        }
+        try {
+            $errorDetailsObject = ($httpErrorObj.ErrorDetails | ConvertFrom-Json)
+            # Make sure to inspect the error result object and add only the error message as a FriendlyMessage.
+            # $httpErrorObj.FriendlyMessage = $errorDetailsObject.message
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails # Temporarily assignment
+        }
+        catch {
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+try {
+    Write-Information 'Starting eCare PUUR account entitlement import'
+
+    # Set authentication headers
+    $accessToken = Get-GenericScimOAuthToken -ClientID $actionContext.Configuration.ClientId -ClientSecret $actionContext.Configuration.ClientSecret -TokenUrl $actionContext.Configuration.tokenUrl
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+    }
+
+    # Get Accounts
+    $importedAccounts = [System.Collections.ArrayList]::new()
+
+    $take = 1000
+    $skip = 1 # SCIM uses 1-based index
+    $moreRecords = $true
+
+    while ($moreRecords) {
+        $splatGetAccounts = @{
+            Uri     = "$($actionContext.Configuration.BaseUrl)/scim/Users?startIndex=$skip&count=$take"
+            Method  = 'GET'
+            Headers = $headers
+        }
+    
+        $GetResponse = (Invoke-EcareRestMethod @splatGetAccounts)
+
+        foreach ($record in $GetResponse.Resources) {
+            [void]$importedAccounts.Add($record)
+        }
+
+        if ($GetResponse.totalResults -lt ($skip + $take - 1)) {
+            $moreRecords = $false
+        }
+        else {
+            $skip += $take
+        }
+    }
+    
+    foreach ($importedAccount in $importedAccounts) {
+        $enabled = $false
+
+        # Only enable if account is active
+        if ($importedAccount.active -eq $true) {
+            $enabled = $true
+        }
+
+        # Set UserName if missing
+        if ([string]::IsNullOrEmpty($importedAccount.userName)) {
+            $importedAccount.userName = $importedAccount.id
+        }
+
+        # Set DisplayName if missing
+        if ([string]::IsNullOrEmpty($importedAccount.displayName)) {
+            $importedAccount.displayName = $importedAccount.userName
+        }
+
+        # Return the result
+        Write-Output @{
+            AccountReference = $importedAccount.id
+            DisplayName      = $importedAccount.displayName
+            UserName         = $importedAccount.userName
+            Enabled          = $enabled
+            Data             = $importedAccount
+        }
+    }
+    
+    Write-Information 'eCare PUUR account entitlement import completed'
+}
+catch {
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-ServiceNowError -ErrorObject $ex
+        Write-Error "Could not import eCare PUUR account entitlements. Error: $($errorObj.FriendlyMessage)"
+        Write-Warning "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    }
+    else {
+        Write-Error "Could not import eCare PUUR account entitlements. Error: $($ex.Exception.Message)"
+        Write-Warning "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+}
